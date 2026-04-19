7d19bcb66ad41b91ba8b03ec51128d22f0e1ec3d
ParamGhetia <paramgghetia@gmail.com>
2026-04-18 22:47:18 -0400

    fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,11 +4,6 @@ export const metadata: Metadata = { title: 'Beacon', description: 'Find your nex
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&family=DM+Sans:wght@300;400;500;600&display=swap" rel="stylesheet" />
-      </head>
       <body>{children}</body>
     </html>
   )
